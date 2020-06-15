d323f8b4556f54a6d468712353532a44d16171ac
Jun Xian <yjxian7@gmail.com>
2020-06-15 16:23:38 +0800

    Added additional comments tests file has some basic input available

--- process.py
+++ process.py
@@ -1,10 +1,13 @@
 import numpy as np
-from copy import copy
 
 np.set_printoptions(suppress=True)
 
 
+# A spectrum class that stores the original spectroscopic information
+# along with the modified spectrum, to be able to return the scale/energy shift
+# If passed a csv file, also stores the column titles
 class Spectrum:
+    # Constructor takes a file path text input, or an array input
     def __init__(self, csvfile):
         if type(csvfile) == str:
             array = np.genfromtxt(csvfile, delimiter=',')
@@ -56,13 +59,16 @@ def scale(data, scaling):
     data.scale(scaling)
 
 
-# Reduces the amount of data in the expected data to facilitate fitting to the observed data
+# Reduces the amount of data in the expected spectrum to facilitate fitting to the observed data
 # To Do: account for missing x-axis if shift is large
 def __match(expected, observed):
+    # Creates an empty array with a size of the final array to be produced
     row_size = np.size(observed.data(), 0)
     column_size = np.size(expected.data(), 1)
     reduced = np.zeros((row_size, column_size), dtype=float)
     i = 0
+    # Searches the expected spectrum for each x-axis point in observed spectrum
+    # Takes the row with that x-axis point and inserts it into the empty array
     for x in observed.data()[:, 0]:
         row = np.where((x + 0.00001 >= expected.data()[:, 0]) & (expected.data()[:, 0] >= x - 0.00001))
         reduced[i] = expected.data()[row, :]
@@ -70,8 +76,10 @@ def __match(expected, observed):
     return Spectrum(reduced)
 
 
-# Trims the observed data to the desired range for fitting
+# Trims the observed spectrum to the desired range for fitting
 def __range_select(spectrum, lower, upper):
+    # Raises exceptions if the range input is invalid
+    # Range has to be two numbers, up to 1 d.p.
     if isinstance(lower, (int, float)) == False or isinstance(upper, (int, float)) == False:
         raise Exception('Please input two numbers (up to one decimal point)')
     elif upper <= lower:
@@ -87,6 +95,8 @@ def __range_select(spectrum, lower, upper):
 
 # Calculates the Rf value of the expected vs observed data
 def residual(expected, observed, lower='min', upper='max'):
+    # If a range has been defined, trim the spectrum to the selected range
+    # Otherwise, use the full range
     if lower != 'min' or upper != 'max':
         try:
             trimmed = __range_select(observed, lower, upper)
@@ -102,10 +112,14 @@ def residual(expected, observed, lower='min', upper='max'):
 
 # Private method for optimising the energy shift automatically
 def __optimise_x(expected, observed, step_size):
+    # Calculates the current Rf and set it as the min for baseline value
+    # Then shifts it by one step and recalculates the Rf
     rf = residual(expected, observed)
     min_rf = float(rf)
     expected.x_shift(step_size)
     rf = residual(expected, observed)
+    # If the Rf is smaller, continue shifting it until the Rf gets bigger
+    # Undo the very last step to get it back to the correct minimum value
     if rf < min_rf:
         while rf < min_rf:
             min_rf = float(rf)
@@ -113,6 +127,8 @@ def __optimise_x(expected, observed, step_size):
             rf = residual(expected, observed)
             if rf > min_rf:
                 expected.x_shift(-step_size)
+    # If the Rf is bigger, shift it the other way and recalculate the Rf as it gets reduced
+    # Once the Rf gets bigger again rather than smaller, undo the very last step
     elif rf > min_rf:
         while rf >= min_rf:
             expected.x_shift(-step_size)
@@ -125,14 +141,20 @@ def __optimise_x(expected, observed, step_size):
     return expected
 
 
-# Private method for optimising the energy shift automatically
+# Private method for optimising the intensity scaling automatically
 def __optimise_scale(expected, observed, step_size):
+    # Calculates the current Rf and set it as the min for baseline value
+    # Also retrieves the current scaling of the spectrum
+    # Changes the scaling of the spectrum by increasing it by one step
+    # Then recalculates the Rf value
     current_scale = expected.get_scale()
     rf = residual(expected, observed)
     min_rf = float(rf)
     current_scale += step_size
     expected.scale(current_scale)
     rf = residual(expected, observed)
+    # If the Rf value is smaller, keep increasing the scaling in step sizes
+    # Until the Rf gets bigger than minimum, then reverse the very last step
     if rf < min_rf:
         while rf < min_rf:
             min_rf = float(rf)
@@ -142,6 +164,10 @@ def __optimise_scale(expected, observed, step_size):
             if rf > min_rf:
                 current_scale -= step_size
                 expected.scale(current_scale)
+    # If the Rf value gets bigger, decrease the scaling in step sizes
+    # Unless doing so would make it a negative scaling, then undo that and stop
+    # Keep decreasing the scaling otherwise until the Rf value gets bigger again
+    # Then reverse the very last step
     elif rf > min_rf:
         while rf >= min_rf:
             current_scale -= step_size
@@ -160,7 +186,7 @@ def __optimise_scale(expected, observed, step_size):
 
 
 # Automatically optimises the energy shift and scaling of the theoretical data
-# To minimise the Rf value when compared to the observed data
+# To minimise the Rf value when compared to a given set of observed data
 def optimise(expected, observed, lower='min', upper='max'):
     if lower != 'min' or upper != 'max':
         try:
@@ -170,10 +196,13 @@ def optimise(expected, observed, lower='min', upper='max'):
     else:
         trimmed = observed
     i = 1
+    # The automatic fitting loops 3 times to ensure good minimisation
+    # Different step sizes called each time just to speed up process
     while i <= 3:
         expected = __optimise_x(expected, trimmed, 1)
         expected = __optimise_x(expected, trimmed, 0.1)
         expected = __optimise_x(expected, trimmed, 0.01)
+        expected = __optimise_scale(expected, trimmed, 100)
         expected = __optimise_scale(expected, trimmed, 10)
         expected = __optimise_scale(expected, trimmed, 1)
         expected = __optimise_scale(expected, trimmed, 0.1)
--- tests.py
+++ tests.py
@@ -1,13 +1,11 @@
 import process
 from process import Spectrum
 
-anatase_theory = Spectrum('anatase_python_test.csv')
-anatase_observed = Spectrum('anatase_observed.csv')
+theory = Spectrum('anatase_python_test.csv')
+observed = Spectrum('anatase_observed.csv')
 
-# anatase_theory.x_shift(2.61)
-# anatase_theory.scale(309.95)
-# print(process.residual(anatase_theory, anatase_observed, 0, 9))
-# print(process.residual(process.optimise(anatase_theory, anatase_observed, 2.6, 0, 9), anatase_observed))
-
-optimised = process.optimise(anatase_theory, anatase_observed, 0, 9)
-print(optimised.data())
+# Change desired range on the right
+optimised = process.optimise(theory, observed, 0, 9)
+print(process.residual(optimised, observed, 0 , 9))
+print(optimised.get_scale())
+print(optimised.get_shift())
